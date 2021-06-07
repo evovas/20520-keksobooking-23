371d2878f602316093d67451ed49689f7200220b
zvovas <vovasevgeniy@gmail.com>
2021-06-07 18:23:40 +0300

    Исправляет ошибки в коде и делает аго лаконичнее

--- js/main.js
+++ js/main.js
@@ -6,9 +6,7 @@ const getRandomDecimalPlace = (min = 0, max = 0, decimalPlaceNumber = 1) => Numb
 
 const getIntWithLeadingZeros = (number, minCountSymbol) => (String(number).length < minCountSymbol) ? '0'.repeat(minCountSymbol - String(number).length) + number : String(number);
 
-const generateAvatars = (count) => Array(count).fill('').map((item, index) => `img/avatars/user${  getIntWithLeadingZeros(index + 1, 2)  }.png`);
-
-function getRandomElements (count, array) {
+function getRandomElements (array, count) {
   const fullArray = array.slice();
   const currentArray = [];
   for (let ind = 0; ind < count; ind++) {
@@ -17,7 +15,9 @@ function getRandomElements (count, array) {
   return currentArray;
 }
 
-const avatars = generateAvatars(10);
+function getRandomElement (array) {
+  return array[getRandomIntInclusive(0, array.length - 1)];
+}
 
 const TYPES_OF_HOUSE = [
   'palace',
@@ -48,23 +48,25 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
+const SIMILAR_ANNOUNCEMENT_COUNT = 10;
+
 function generateAnnouncement (index) {
   const announcement = {
     author : {
-      avatar : avatars[index],
+      avatar : `img/avatars/user${  getIntWithLeadingZeros(index + 1, 2)  }.png`,
     },
     offer : {
       title : `Объявление об аренде №${  index + 1}`,
       address : '',
       price : getRandomIntInclusive(10000, 100000),
-      type : TYPES_OF_HOUSE[getRandomIntInclusive(0, TYPES_OF_HOUSE.length - 1)],
+      type : getRandomElement(TYPES_OF_HOUSE),
       rooms : getRandomIntInclusive(1, 10),
       guests : getRandomIntInclusive(1, 10),
-      checkin : CHECK_IN_OUT_TIME[getRandomIntInclusive(0, CHECK_IN_OUT_TIME.length - 1)],
-      checkout : CHECK_IN_OUT_TIME[getRandomIntInclusive(0, CHECK_IN_OUT_TIME.length - 1)],
-      features : getRandomElements(getRandomIntInclusive(0, FEATURES_OF_HOUSE.length), FEATURES_OF_HOUSE),
+      checkin : getRandomElement(CHECK_IN_OUT_TIME),
+      checkout : getRandomElement(CHECK_IN_OUT_TIME),
+      features : getRandomElements(FEATURES_OF_HOUSE, getRandomIntInclusive(0, FEATURES_OF_HOUSE.length)),
       description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      photos : getRandomElements(getRandomIntInclusive(0, PHOTOS.length), PHOTOS),
+      photos : getRandomElements(PHOTOS, getRandomIntInclusive(0, PHOTOS.length)),
     },
     location : {
       lat : getRandomDecimalPlace(35.65, 35.7, 5),
@@ -75,9 +77,7 @@ function generateAnnouncement (index) {
   return announcement;
 }
 
-const announcements = Array(10).fill('').map((item, index) => generateAnnouncement(index));
+const announcements = new Array(SIMILAR_ANNOUNCEMENT_COUNT).fill('').map((item, index) => generateAnnouncement(index));
 
 //Код для прохождения проверки линтером
 announcements.slice();
-getRandomIntInclusive(1, 3);
-getRandomDecimalPlace(1.1, 1.2, 1);

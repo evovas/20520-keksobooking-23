7696261cc6ee61c2ee74e037704c04ba45eaba6c
zvovas <vovasevgeniy@gmail.com>
2021-06-09 12:10:03 +0300

    Вносит исправления в генерацию данных по комментариям наставника

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
-const getRandomNumber = (min = 0, max = 0) => Math.random() * (max - min) + min;
-
-const getRandomIntInclusive = (min = 0, max = 0) => Math.floor(getRandomNumber(min, max + 1));
-
-const getRandomDecimalPlace = (min = 0, max = 0, decimalPlaceNumber = 1) => Number(getRandomNumber(min, max).toFixed(decimalPlaceNumber));
-
-const getIntWithLeadingZeros = (number, minCountSymbol) => (String(number).length < minCountSymbol) ? '0'.repeat(minCountSymbol - String(number).length) + number : String(number);
-
-function getRandomElements (array, count) {
-  const fullArray = array.slice();
-  const currentArray = [];
-  for (let ind = 0; ind < count; ind++) {
-    currentArray.push(fullArray.splice(getRandomIntInclusive(0, fullArray.length - 1), 1));
-  }
-  return currentArray;
-}
-
-function getRandomElement (array) {
-  return array[getRandomIntInclusive(0, array.length - 1)];
-}
+const SIMILAR_ANNOUNCEMENT_COUNT = 10;
+const MINIMUM_GENERATE_PRICE = 10000;
+const MAXIMUM_GENERATE_PRICE = 100000;
+const MINIMUM_GENERATE_ROOMS = 1;
+const MAXIMUM_GENERATE_ROOMS = 10;
+const MINIMUM_GENERATE_GUEST = 1;
+const MAXIMUM_GENERATE_GUEST = 10;
 
 const TYPES_OF_HOUSE = [
   'palace',
@@ -48,36 +35,58 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-const SIMILAR_ANNOUNCEMENT_COUNT = 10;
+const getRandomNumber = (min = 0, max = 0) => Math.random() * (max - min) + min;
 
-function generateAnnouncement (index) {
-  const announcement = {
+const getRandomInt = (min = 0, max = 0) => Math.floor(getRandomNumber(min, max + 1));
+
+const getRandomDecimalPlace = (min = 0, max = 0, decimalPlaceNumber = 1) => Number(getRandomNumber(min, max).toFixed(decimalPlaceNumber));
+
+const getRandomElement = function (array) {
+  return array[getRandomInt(0, array.length - 1)];
+};
+
+const getRandomElements = function (array = [], count = 0) {
+  const randomElements = new Set();
+  if (count || array.length === 0) {
+    for (let ind = 0; ind < count; ind++) {
+      randomElements.add(getRandomElement(array));
+    }
+  }
+  return Array.from(randomElements);
+};
+
+
+const generateAnnouncement = function (index) {
+  const lat = getRandomDecimalPlace(35.65, 35.7, 5);
+  const lng = getRandomDecimalPlace(139.7, 139.8, 5);
+  return  {
     author : {
-      avatar : `img/avatars/user${  getIntWithLeadingZeros(index + 1, 2)  }.png`,
+      avatar : `img/avatars/user${  (index < 9 ? `0${  index + 1}` : index + 1)  }.png`,
     },
     offer : {
-      title : `Объявление об аренде №${  index + 1}`,
-      address : '',
-      price : getRandomIntInclusive(10000, 100000),
+      title : `Объявление об аренде №${index + 1}`,
+      address : `${lat}, ${lng}`,
+      price : getRandomInt(MINIMUM_GENERATE_PRICE, MAXIMUM_GENERATE_PRICE),
       type : getRandomElement(TYPES_OF_HOUSE),
-      rooms : getRandomIntInclusive(1, 10),
-      guests : getRandomIntInclusive(1, 10),
+      rooms : getRandomInt(MINIMUM_GENERATE_ROOMS, MAXIMUM_GENERATE_ROOMS),
+      guests : getRandomInt(MINIMUM_GENERATE_GUEST, MAXIMUM_GENERATE_GUEST),
       checkin : getRandomElement(CHECK_IN_OUT_TIME),
       checkout : getRandomElement(CHECK_IN_OUT_TIME),
-      features : getRandomElements(FEATURES_OF_HOUSE, getRandomIntInclusive(0, FEATURES_OF_HOUSE.length)),
+      features : getRandomElements(FEATURES_OF_HOUSE, getRandomInt(0, FEATURES_OF_HOUSE.length)),
       description : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      photos : getRandomElements(PHOTOS, getRandomIntInclusive(0, PHOTOS.length)),
+      photos : getRandomElements(PHOTOS, getRandomInt(0, PHOTOS.length)),
     },
     location : {
-      lat : getRandomDecimalPlace(35.65, 35.7, 5),
-      lng : getRandomDecimalPlace(139.7, 139.8, 5),
+      lat : lat,
+      lng : lng,
     },
   };
-  announcement.offer.address = `${announcement.location.lat  }, ${  announcement.location.lng}`;
-  return announcement;
-}
+};
 
-const announcements = new Array(SIMILAR_ANNOUNCEMENT_COUNT).fill('').map((item, index) => generateAnnouncement(index));
+const announcements = [];
+for (let ind = 0; ind < SIMILAR_ANNOUNCEMENT_COUNT; ind++) {
+  announcements.push(generateAnnouncement(ind));
+}
 
 //Код для прохождения проверки линтером
 announcements.slice();

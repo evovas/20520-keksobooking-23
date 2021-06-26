7a694017bde2d19620f476b78c51a8a8fa49695b
zvovas <vovasevgeniy@gmail.com>
2021-06-26 20:53:31 +0300

    Добавляет возможность делать страницу активной и неактивной

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
+import {enableActivePageState, enableInactivePageState} from './toggle-page-state.js';
 import {announcementsData} from './data.js';
 import {createAnnouncementCard} from './create-announcement-card.js';
 
 //Проверка работы
+enableInactivePageState();
+enableActivePageState();
 document.querySelector('#map-canvas').appendChild(createAnnouncementCard(announcementsData[7]));
